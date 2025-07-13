2f788ba4aa55fbe7a6e6886fbb1521b4219057bb
de-sam <samsoncoded@gmail.com>
2025-07-13 10:26:16 +0100

    python decorators

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.conn:
+            self.conn.close()
+
+if __name__ == "__main__":
+    with DatabaseConnection('users.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users")
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
