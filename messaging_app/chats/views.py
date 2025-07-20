e19c1513cf0a978eeb8c02fd0da357671077fd4b
de-sam <samsoncoded@gmail.com>
2025-07-20 13:39:10 +0100

    added serializers

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+#!/usr/bin/env python3
+"""Viewsets for messaging app"""
 
-# Create your views here.
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    """ViewSet for managing conversations"""
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        """When a conversation is created, add the requesting user as a participant"""
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
+        conversation.save()
+
+    @action(detail=True, methods=['post'])
+    def add_participant(self, request, pk=None):
+        """Add a participant to the conversation"""
+        conversation = self.get_object()
+        user = request.user
+        conversation.participants.add(user)
+        conversation.save()
+        return Response({'status': 'participant added'})
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """ViewSet for managing messages"""
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        """Set sender as the requesting user"""
+        serializer.save(sender=self.request.user)
