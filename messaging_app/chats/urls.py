eec9c7ead01403fc07bd75ae3fb0cc91f885c926
de-sam <samsoncoded@gmail.com>
2025-07-20 13:44:27 +0100

    added viewsets and serializsers

---
+++ messaging_app/chats/urls.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""URLs for chats app"""
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ConversationViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='message')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
