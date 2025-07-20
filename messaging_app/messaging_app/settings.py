a4d4ff52dc322fe8bc4e53a6a7997975084f8ddb
de-sam <samsoncoded@gmail.com>
2025-07-20 12:57:42 +0100

    updated authentication and school profile page

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -69,11 +69,11 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
     ],
-    'DEFAULT_PARSER_CLASSES': [
-        'rest_framework.parsers.JSONParser',
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
