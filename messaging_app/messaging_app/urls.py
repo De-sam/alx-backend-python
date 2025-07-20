e181bd2b69f3ece14488e61b9d4848205bc846da
de-sam <samsoncoded@gmail.com>
2025-07-20 14:15:40 +0100

    added viewsets and serializsers

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -6,5 +6,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),  # ✅ Mount all API endpoints under /api/
+    path('api/', include('chats.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
