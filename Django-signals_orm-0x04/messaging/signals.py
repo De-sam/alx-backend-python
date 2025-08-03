e9488e756ebe2e76dc40bdd496029f860afd527d
de-sam <samsoncoded@gmail.com>
2025-08-03 16:41:14 +0100

    Enhaced mobile view

---
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -0,0 +1,33 @@
+"""
+This file defines signal handlers for the chats app.
+
+Signal:
+    post_save (Message) – Triggers when a new message is created and sends a notification to the receiver.
+"""
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+from chats.models import Message, Notification
+
+
+@receiver(post_save, sender=Message)
+def notify_receiver_on_new_message(sender, instance: Message, created: bool, **kwargs):
+    """
+    Creates a Notification for the receiver when a new Message is sent.
+
+    Args:
+        sender (Model class): The model class that sent the signal.
+        instance (Message): The actual instance being saved.
+        created (bool): A boolean indicating if the instance was created.
+        **kwargs: Additional keyword arguments.
+
+    Behavior:
+        - If a new Message is created, a corresponding Notification is created
+          for the `receiver` of the message.
+    """
+    if created:
+        Notification.objects.create(
+            user=instance.receiver,
+            message=instance
+        )
